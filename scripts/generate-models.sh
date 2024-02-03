121fc19f4e3c0e6faa077111662dbb27a5018bcb
Daniel Bluhm <dbluhm@pm.me>
2024-02-02 19:22:47 -0500

    feat: updates
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- scripts/generate-models.sh
+++ scripts/generate-models.sh
@@ -5,21 +5,30 @@ cd "$(dirname "$0")" || exit
 
 CONTAINER_RUNTIME=${CONTAINER_RUNTIME:-docker}
 NAME="datamodel-codegen"
-API_URL="https://raw.githubusercontent.com/Indicio-tech/acapy-openapi/main/openapi.yml"
+VERSION="${1:-main}"
+if [ $# -gt 0 ]; then
+    shift
+fi
+API_URL="${API_URL:-https://raw.githubusercontent.com/hyperledger/aries-cloudagent-python/${VERSION}/open-api/openapi.json}"
 
 ${CONTAINER_RUNTIME} build -t ${NAME} - << DOCKERFILE
-FROM python:3.7
+FROM python:3.10
 
 WORKDIR /usr/src/app
 
-RUN pip install datamodel-code-generator[http]==0.13.0
+RUN pip install datamodel-code-generator[http]==0.25.1
 
 ENTRYPOINT ["datamodel-codegen"]
 DOCKERFILE
 
-${CONTAINER_RUNTIME} run --rm -it -v "$(realpath $(pwd)/../):/usr/src/app:z" ${NAME} \
+${CONTAINER_RUNTIME} run --rm -v "$(pwd)/../:/usr/src/app:z" ${NAME} \
     --url "${API_URL}" \
     --output ./src/models.py \
     --field-constraints \
     --use-schema-description \
-    --reuse-model
+    --enum-field-as-literal all \
+    --reuse-model \
+    --snake-case-field \
+    --allow-population-by-field-name \
+    --aliases ./scripts/aliases.json \
+    $@
